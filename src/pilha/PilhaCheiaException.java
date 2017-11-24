adb76f502cdf0f3d4370be739bf888b6b8e518a6
Gerdon Mafra <gerdon_mafra@hotmail.com>
2017-11-23 23:56:40 -0300

    Pilha

---
+++ src/pilha/PilhaCheiaException.java
@@ -0,0 +1,13 @@
+package pilha;
+
+public class PilhaCheiaException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public PilhaCheiaException(String msg){
+		super(msg);
+	}
+}
